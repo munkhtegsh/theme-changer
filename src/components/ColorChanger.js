9783a32b8cae00e40db34b5481317c4033e11d05
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2017-12-13 20:29:29 -0800

    Finished the project

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -9,10 +9,18 @@ export default class ColorChanger extends Component {
   }
 
   // componentWillReceiveProps
+  componentWillReceiveProps(props){
+    this.setState({
+      allowEdit : props.allowEdit
+    });
+  }
+  
 
   render() {
     return (
-      <select className="dropDownContainer">
+      <select className="dropDownContainer" 
+      onChange={(e)=>this.props.update(e.target.value)}
+      disabled={this.props.allowEdit === 'false'}>
         <option value="black"> Black </option>
         <option value="blue"> Blue </option>
         <option value="green"> Green </option>
